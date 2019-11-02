27f210787fff730f7a22458d72bbc99e9c2b0ce0
txya900619 <wayne900619@gmail.com>
2019-11-02 16:35:13 +0800

    add template

--- fileStruct/controller/home.go
+++ fileStruct/controller/home.go
@@ -10,9 +10,15 @@ type home struct{}
 
 func (h home) registerRoutes() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("range", rangeHandler)
 }
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	vop := vm.IndexViewModelOp{}
 	v := vop.GetVM()
 	templates["index.html"].Execute(w, &v)
+}
+func rangeHandler(w http.ResponseWriter, r *http.Request) {
+	vop := vm.IndexViewModelOp{}
+	v := vop.GetVM()
+	templates["range.html"].Execute(w, &v)
 }
